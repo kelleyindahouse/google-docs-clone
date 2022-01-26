2b04dc6ed1e8373e572e496505d5475481d63575
kelleyindahouse <91229799+kelleyindahouse@users.noreply.github.com>
2022-01-26 02:03:24 -0500

    :(

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log('MongoDB connected!'))
   .catch(err => console.log('Error:- ' + err))
 
-const io = require("socket.io")("https://lofi-study-room.herokuapp.com/")
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "https://lofi-study-room.herokuapp.com/",
+    methods: ["GET", "POST"]
+  }
+})
 
 const defaultValue = ''
 
@@ -47,10 +52,3 @@ async function findOrCreateDocument(id) {
   if (document) return document
   return await Document.create({ _id: id, data: defaultValue })
 }
-
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));

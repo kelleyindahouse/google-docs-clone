259675d53a068052cf28b0b7bbdc1144c7c4d9b9
kelleyindahouse <91229799+kelleyindahouse@users.noreply.github.com>
2022-01-26 01:35:06 -0500

    what is going on

--- server.js
+++ server.js
@@ -45,3 +45,10 @@ async function findOrCreateDocument(id) {
   if (document) return document
   return await Document.create({ _id: id, data: defaultValue })
 }
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));

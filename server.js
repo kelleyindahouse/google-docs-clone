f0f0f0b01e7a70188a65085126983c397cc1e672
kelleyindahouse <91229799+kelleyindahouse@users.noreply.github.com>
2022-01-26 00:57:41 -0500

    commented out code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Document = require('./Document')
 
-mongoose.connect('mongodb://localhost:27017/lofinotes');
+// mongoose.connect('mongodb://localhost:27017/lofinotes');
 
 const uri = process.env.MONGODB_URI;
 

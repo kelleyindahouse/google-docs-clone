7adb5cbf0859c13110878d3ebe4610271ca6e3cd
kelleyindahouse <91229799+kelleyindahouse@users.noreply.github.com>
2022-01-26 02:47:50 -0500

    whaaat

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const cors = require('cors')
 const server = require('http').createServer(app)
 const PORT = process.env.PORT || 5000
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('clientside/build'))
-}
-
 app.use(cors())
 
 const io = require('socket.io')(server)
